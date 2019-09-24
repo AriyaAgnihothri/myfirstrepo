dbce0d5e1d6d360e4b6372ca07ac80ac0c480adf
user40 <user40@user40-G41T-M7>
2019-09-24 15:01:19 +0530

    firstcommit

---
+++ SalesToolv2019/src/com/ust/test/Main.java
@@ -0,0 +1,22 @@
+package com.ust.test;
+
+import java.awt.DisplayMode;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData objSalesData= new SalesData();
+		System.out.println("Welcome to Sales App");
+		displayGreeting();
+//		objSalesData.display();
+		
+
+	}
+	public static void  displayGreeting() {
+		System.out.println("Static Function Display");
+		System.out.println();
+		
+	}
+
+}
